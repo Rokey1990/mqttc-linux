f6f2d208ceff3322b8ba117a3bb442ec0f941529
Rokey1990 <992030970@qq.com>
2016-04-13 10:24:29 +0800

    Initial Commit

---
+++ MqttCommand/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MqttCommand
+//
+//  Created by anjubao on 4/13/16.
+//  Copyright © 2016 anjubao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
